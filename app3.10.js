f01875d4aa34cf569921891da125e92a17d14889
Tien Nguyen-Borland <tnguyenborland@tnguyenborland-mac.local>
2023-02-28 09:02:52 -0700

    importing npm modules

--- app3.10.js
+++ app3.10.js
@@ -12,9 +12,13 @@
 //3. Export getNotes function
 //4. From app.js load in and call the function printing message to console
 
+const validator = require('validator')
 
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 
 console.log(msg)
+
+console.log(validator.isURL('http//mead.io'))
+console.log(validator.isEmail('tien@example.com'))
